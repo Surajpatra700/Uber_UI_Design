7e66fa4493b1d36d7ccfed8c338aa4d923807905
Sujal Nath <sujalnath108@gmail.com>
2023-07-02 03:10:04 +0530

    Screen3 completed and added images and icons in assets

---
+++ lib/screen3.dart
@@ -0,0 +1,290 @@
+import 'package:flutter/material.dart';
+
+class Screen3 extends StatelessWidget {
+  const Screen3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+      children: [
+        Image.asset('assets/img/map.jpg'),
+        Padding(
+          padding: const EdgeInsets.only(top: 210.0),
+          child: Container(
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(30.0),
+                topLeft: Radius.circular(30.0),
+              ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 230.0),
+          child: Container(
+            child: Column(
+              children: [
+                const Align(
+                  alignment: Alignment.center,
+                  child: Text("Book a ride",
+                      textAlign: TextAlign.end,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25,
+                      )),
+                ),
+                const Divider(
+                  thickness: 2,
+                ),
+                Container(
+                  margin: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(
+                      width: 3,
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        Container(
+                            width: 400,
+                            height: 125,
+                            decoration: const BoxDecoration(
+                                shape: BoxShape.rectangle,
+                                image: DecorationImage(
+                                    fit: BoxFit.contain,
+                                    image:
+                                        AssetImage('assets/icon/ic_cab.png')))),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text(
+                                  "Uber Go",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 25,
+                                  ),
+                                ),
+                                const Text(
+                                  "8:46pm - 4min away",
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                  ),
+                                ),
+                                Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.blueAccent,
+                                    borderRadius: BorderRadius.circular(5),
+                                  ),
+                                  child: const Padding(
+                                    padding: EdgeInsets.only(left: 5, right: 5),
+                                    child: Text("* Faster",
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.normal,
+                                          fontSize: 18,
+                                        )),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const Text("\u{20B9}${179}",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 25,
+                                )),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(left: 7, right: 7, top: 5),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                          width: 100,
+                          height: 100,
+                          decoration: const BoxDecoration(
+                              shape: BoxShape.rectangle,
+                              image: DecorationImage(
+                                  fit: BoxFit.contain,
+                                  image:
+                                      AssetImage('assets/icon/ic_auto.png')))),
+                      const Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Uber Go",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 25,
+                              ),
+                            ),
+                            Text(
+                              "8:46pm - 4 min away",
+                              style: TextStyle(
+                                fontSize: 18,
+                              ),
+                            ),
+                          ]),
+                      Column(
+                        children: [
+                          Container(
+                            child: Row(children: [
+                              Container(
+                                  width: 20,
+                                  height: 20,
+                                  decoration: const BoxDecoration(
+                                      shape: BoxShape.rectangle,
+                                      image: DecorationImage(
+                                          fit: BoxFit.contain,
+                                          image: AssetImage(
+                                              'assets/icon/ic_priceTag.png')))),
+                              const Text("\u{20B9}${170.71}",
+                                  style: TextStyle(
+                                    fontSize: 25,
+                                    fontWeight: FontWeight.bold,
+                                  )),
+                            ]),
+                          ),
+                          Text(
+                            "\u{20B9}${188.71}",
+                            style: TextStyle(
+                              fontSize: 18,
+                              decoration: TextDecoration.lineThrough,
+                              color: Colors.black.withOpacity(0.7),
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(left: 7, right: 7, top: 5),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                          width: 100,
+                          height: 100,
+                          decoration: const BoxDecoration(
+                              shape: BoxShape.rectangle,
+                              image: DecorationImage(
+                                  fit: BoxFit.contain,
+                                  image: AssetImage(
+                                      'assets/icon/ic_uberPremiere.png')))),
+                      const Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Uber Premiere",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 25,
+                              ),
+                            ),
+                            Text(
+                              "8:46pm - 5 min away",
+                              style: TextStyle(
+                                fontSize: 18,
+                              ),
+                            ),
+                          ]),
+                      const Text("\u{20B9}${170.71}",
+                          style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                          )),
+                    ],
+                  ),
+                ),
+                const Divider(
+                  thickness: 2,
+                ),
+                Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(left: 10),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                                width: 50,
+                                height: 30,
+                                decoration: const BoxDecoration(
+                                    shape: BoxShape.rectangle,
+                                    image: DecorationImage(
+                                        fit: BoxFit.contain,
+                                        image: AssetImage(
+                                            'assets/icon/ic_gpayLogo.png')))),
+                            const Padding(
+                              padding: EdgeInsets.only(left: 10),
+                              child: Text(
+                                "zairza.outr@hdfcbank",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 22,
+                                ),
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.topRight,
+                              child: Container(
+                                  width: 30,
+                                  height: 30,
+                                  decoration: const BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      image: DecorationImage(
+                                          fit: BoxFit.cover,
+                                          image: AssetImage(
+                                              'assets/icon/ic_forward-arrow.png')))),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        width: 350,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          color: Colors.black,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: const Padding(
+                          padding: EdgeInsets.only(top: 5, bottom: 5),
+                          child: Align(
+                            alignment: Alignment.center,
+                            child: Text("Choose Uber Go",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.normal,
+                                  fontSize: 28,
+                                  color: Colors.white,
+                                )),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
+    ));
+  }
+}
